7ce777d29955f4fc5092f80c2f19c2b15ac65f6a
QuangTien <nguyenquangtienvn2022@gmail.com>
2024-08-30 14:53:02 +0700

    apisettingaxios

---
+++ package/src/app/services/providerService.tsx
@@ -0,0 +1,7 @@
+import axiosInstance from "./axiosInstance";
+
+
+export const getProvider = async () => {
+    const response = await axiosInstance.get(`/sanpham`);
+    return response.data;
+  };
