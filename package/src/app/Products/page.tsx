e62393a252ba76958d6a54de630268c82f549e91
QuangTien <nguyenquangtienvn2022@gmail.com>
2024-09-04 16:14:37 +0700

    dev ui table

--- package/src/app/Products/page.tsx
+++ package/src/app/Products/page.tsx
@@ -1,18 +1,45 @@
 'use client'
-import { Grid, Box } from '@mui/material';
-import PageContainer from '@/app/components/container/PageContainer';
-// components
-import SalesOverview from '@/app/components/dashboard/SalesOverview';
-import YearlyBreakup from '@/app/components/dashboard/YearlyBreakup';
-import RecentTransactions from '@/app/components/dashboard/RecentTransactions';
-import ProductPerformance from '@/app/components/dashboard/ProductPerformance';
-import Blog from '@/app/components/dashboard/Blog';
-import MonthlyEarnings from '@/app/components/dashboard/MonthlyEarnings';
-import TableComponent from'../components/shared/TableComponet';
+import TableComponet from '../components/shared/TableComponet';
+
+import { GridColDef } from '@mui/x-data-grid';
+import { useEffect, useState } from 'react';
+import { getProvider } from '../services/providerService';
+import { LinearProgress, Box } from '@mui/material';
+
+const columns: GridColDef[] = [
+  { field: '  ', headerName: 'ID', width: 70 },
+  { field: 'name', headerName: 'Name', width: 200 },
+  { field: 'hello', headerName: 'Hello', type: 'number', width: 100 },
+  { field: 'number', headerName: 'Category', width: 130 },
+  { field: 'invoices', headerName: 'Invoices', width: 130 },
+];
+
 
 function Product() {
+  const [rows, setRows] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const products = await getProvider();
+        setRows(products);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  if (loading) {
+    return <Box sx={{ width: '100%' }}><LinearProgress /></Box>;
+  }
+
   return (
-    <TableComponent />
+    <TableComponet columns={columns} rows={rows} />
   );
 }
 
--- package/src/app/services/providerService.tsx
+++ package/src/app/services/providerService.tsx
@@ -2,6 +2,6 @@ import axiosInstance from "./axiosInstance";
 
 
 export const getProvider = async () => {
-    const response = await axiosInstance.get(`/sanpham`);
-    return response.data;
-  };
+  const response = await axiosInstance.get(`/sanpham`);
+  return response.data;
+};
